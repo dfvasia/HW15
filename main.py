396e87803668c960529b571807e7dc35cc1178f4
Василий <vasilii-zhuravsk@mail.ru>
2022-01-18 17:28:42 +0300

    first

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, abort, jsonify
+import os
 
 app = Flask(__name__)
 
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+DBNAME = f"{BASEDIR}\\netflix.db"
+
+
 @app.route('/')
 def start_page():
-    pass
+    s = {
+        'title': 1,
+        'genre': 2,
+        'description': 3,
+        'country': 4,
+        'release_year': 5
+    }
+    return render_template('found.html', s=s)
 
 
 if __name__ == '__main__':
-    app.run(debag=True)
+    app.run(debug=True)
